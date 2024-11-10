4d959724efc0015196423184dece0ac870fd41db
Железнякович Максим <m.zheleznyakovich@bft.ru>
2024-11-10 19:38:00 +0300

    adding book-service

---
+++ book-service/app/api/db.py
@@ -0,0 +1,24 @@
+import os
+
+from sqlalchemy import (Column, Integer, MetaData, String, Table,
+                        create_engine, ARRAY, Float)
+
+from databases import Database
+
+DATABASE_URI = os.getenv('DATABASE_URI')
+
+engine = create_engine(DATABASE_URI)
+metadata = MetaData()
+
+books = Table(
+    'books',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String(50)),
+    Column('author', String(30)),
+    Column('genre', String(30)),
+    Column('price', Float),
+    Column('description', String(60))
+)
+
+database = Database(DATABASE_URI)
